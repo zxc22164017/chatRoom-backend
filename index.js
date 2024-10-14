c60a3266c8e8504ec9d36367af72194f34234883
Chang ShengWen <asdzxc1234448@gmail.com>
2024-10-14 17:44:22 +0800

    hot fix

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get("/", (req, res) => server.status(200).send("hello"));
+app.get("/", (req, res) => res.status(200).send("hello"));
 app.use("/user", userRouter);
 app.use(requireAuth);
 app.use("/room", roomRouter);
